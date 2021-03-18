4cd747d0e7b9209ffe690ad0b4f176090c1514d2
polio14bmw <eduardocarrascalpablo@hotmail.com>
2021-03-17 21:38:54 -0500

    primer commit

---
+++ Assets/js/Teclado.js
@@ -0,0 +1,357 @@
+
+let casillaLet = document.getElementById("casillaLet")
+/* Numeros */
+let btnNum1 = document.getElementById("btnNum1")
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+/* Letras */
+let btnLetq = document.getElementById("btnLetq")
+let btnLetw = document.getElementById("btnLetw")
+let btnLete = document.getElementById("btnLete")
+let btnLetr = document.getElementById("btnLetr")
+let btnLett = document.getElementById("btnLett")
+let btnLety = document.getElementById("btnLety")
+let btnLetu = document.getElementById("btnLetu")
+let btnLeti = document.getElementById("btnLeti")
+let btnLeto = document.getElementById("btnLeto")
+let btnLetp = document.getElementById("btnLetp")
+let btnLeta = document.getElementById("btnLeta")
+let btnLets = document.getElementById("btnLets")
+let btnLetd = document.getElementById("btnLetd")
+let btnLetf = document.getElementById("btnLetf")
+let btnLetg = document.getElementById("btnLetg")
+let btnLeth = document.getElementById("btnLeth")
+let btnLetj = document.getElementById("btnLetj")
+let btnLetk = document.getElementById("btnLetk")
+let btnLetl = document.getElementById("btnLetl")
+let btnLetñ = document.getElementById("btnLetñ")
+let btnLetz = document.getElementById("btnLetz")
+let btnLetx = document.getElementById("btnLetx")
+let btnLetc = document.getElementById("btnLetc")
+let btnLetv = document.getElementById("btnLetv")
+let btnLetb = document.getElementById("btnLetb")
+let btnLetn = document.getElementById("btnLetn")
+let btnLetm = document.getElementById("btnLetm")
+let btnComa = document.getElementById("btnComa")
+let btnPunto = document.getElementById("btnPunto")
+let btnLinea = document.getElementById("btnLinea")
+let btnEspacio = document.getElementById("btnEspacio")
+let btnEnter = document.getElementById("btnEnter")
+/* Temporales */
+let teclaTemp = ""
+
+/* Funciones numero */
+const btnNum1F = () => {
+casillaLet.innerHTML = teclaTemp + "1";
+teclaTemp = teclaTemp + "1";
+console.log(casillaLet);
+}
+const btnNum2F = () => {
+casillaLet.innerHTML = teclaTemp + "2"
+teclaTemp = teclaTemp + "2"
+}
+const btnNum3F = () => {
+    casillaLet.innerHTML = teclaTemp + "3"
+    teclaTemp = teclaTemp + "3"
+}
+const btnNum4F = () => {
+    casillaLet.innerHTML = teclaTemp + "4"
+    teclaTemp = teclaTemp + "4"
+}
+const btnNum5F = () => {
+    casillaLet.innerHTML = teclaTemp + "5"
+    teclaTemp = teclaTemp + "5"
+}
+const btnNum6F = () => {
+    casillaLet.innerHTML = teclaTemp + "6"
+    teclaTemp = teclaTemp + "6"
+}
+const btnNum7F = () => {
+    casillaLet.innerHTML = teclaTemp + "7"
+    teclaTemp = teclaTemp + "7"
+}
+const btnNum8F = () => {
+    casillaLet.innerHTML = teclaTemp + "8"
+    teclaTemp = teclaTemp + "8"
+}
+const btnNum9F = () => {
+    casillaLet.innerHTML = teclaTemp + "9"
+    teclaTemp = teclaTemp + "9"
+}
+const btnNum0F = () => {
+    casillaLet.innerHTML = teclaTemp + "0"
+    teclaTemp = teclaTemp + "0"
+    }
+/* Funciones letras */
+const btnLetqF = () => {
+    casillaLet.innerHTML = teclaTemp + "q"
+    teclaTemp = teclaTemp + "q"
+}
+const btnLetwF = () => {
+    casillaLet.innerHTML = teclaTemp + "w"
+    teclaTemp = teclaTemp + "w"
+}
+const btnLeteF = () => {
+    casillaLet.innerHTML = teclaTemp + "e"
+    teclaTemp = teclaTemp + "e"
+}
+const btnLetrF = () => {
+    casillaLet.innerHTML = teclaTemp + "r"
+    teclaTemp = teclaTemp + "r"
+}
+const btnLettF = () => {
+    casillaLet.innerHTML = teclaTemp + "t"
+    teclaTemp = teclaTemp + "t"
+}
+const btnLetyF = () => {
+    casillaLet.innerHTML = teclaTemp + "y"
+    teclaTemp = teclaTemp + "y"
+}
+const btnLetuF = () => {
+    casillaLet.innerHTML = teclaTemp + "u"
+    teclaTemp = teclaTemp + "u"
+}
+const btnLetiF = () => {
+    casillaLet.innerHTML = teclaTemp + "i"
+    teclaTemp = teclaTemp + "i"
+}
+const btnLetoF = () => {
+    casillaLet.innerHTML = teclaTemp + "o"
+    teclaTemp = teclaTemp + "o"
+}
+const btnLetpF = () => {
+    casillaLet.innerHTML = teclaTemp + "p"
+    teclaTemp = teclaTemp + "p"
+}
+const btnLetaF = () => {
+    casillaLet.innerHTML = teclaTemp + "a"
+    teclaTemp = teclaTemp + "a"
+}
+const btnLetsF = () => {
+    casillaLet.innerHTML = teclaTemp + "s"
+    teclaTemp = teclaTemp + "s"
+}
+const btnLetdF = () => {
+    casillaLet.innerHTML = teclaTemp + "d"
+    teclaTemp = teclaTemp + "d"
+}
+const btnLetfF = () => {
+    casillaLet.innerHTML = teclaTemp + "f"
+    teclaTemp = teclaTemp + "f"
+}
+const btnLetgF = () => {
+    casillaLet.innerHTML = teclaTemp + "g"
+    teclaTemp = teclaTemp + "g"
+}
+const btnLethF = () => {
+    casillaLet.innerHTML = teclaTemp + "h"
+    teclaTemp = teclaTemp + "h"
+}
+const btnLetjF = () => {
+    casillaLet.innerHTML = teclaTemp + "j"
+    teclaTemp = teclaTemp + "j"
+}
+const btnLetkF = () => {
+    casillaLet.innerHTML = teclaTemp + "k"
+    teclaTemp = teclaTemp + "k"
+}
+const btnLetlF = () => {
+    casillaLet.innerHTML = teclaTemp + "l"
+    teclaTemp = teclaTemp + "l"
+}
+const btnLetñF = () => {
+    casillaLet.innerHTML = teclaTemp + "ñ"
+    teclaTemp = teclaTemp + "ñ"
+}
+const btnLetzF = () => {
+    casillaLet.innerHTML = teclaTemp + "z"
+    teclaTemp = teclaTemp + "z"
+}
+const btnLetxF = () => {
+    casillaLet.innerHTML = teclaTemp + "x"
+    teclaTemp = teclaTemp + "x"
+}
+const btnLetcF = () => {
+    casillaLet.innerHTML = "c"
+    casillaLet.innerHTML = teclaTemp + "c"
+    teclaTemp = teclaTemp + "c"
+}
+const btnLetvF = () => {
+    casillaLet.innerHTML = "v"
+    casillaLet.innerHTML = teclaTemp + "v"
+    teclaTemp = teclaTemp + "v"
+}
+const btnLetbF = () => {
+    casillaLet.innerHTML = "b"
+    casillaLet.innerHTML = teclaTemp + "b"
+    teclaTemp = teclaTemp + "b"
+}
+const btnLetnF = () => {
+    casillaLet.innerHTML = "n"
+    casillaLet.innerHTML = teclaTemp + "n"
+    teclaTemp = teclaTemp + "n"
+}
+const btnLetmF = () => {
+    casillaLet.innerHTML = "m"
+    casillaLet.innerHTML = teclaTemp + "m"
+    teclaTemp = teclaTemp + "m"
+}
+/* Punto,coma y linea */
+const btnComaF = () => {
+    casillaLet.innerHTML = teclaTemp + ","
+    teclaTemp = teclaTemp + ","
+}
+const btnPuntoF = () => {
+    casillaLet.innerHTML = teclaTemp + "."
+    teclaTemp = teclaTemp + "."
+}
+const btnLineaF = () => {
+    casillaLet.innerHTML = teclaTemp + "-"
+    teclaTemp = teclaTemp + "-"
+}
+const btnEspacioF = () => {
+    casillaLet.innerHTML = teclaTemp + " "
+    teclaTemp = teclaTemp + " "
+}
+const btnEnterF = () => {
+casillaLet.innerHTML + "enter"
+teclaTemp = teclaTemp + "enter"
+}
+
+
+/* Onclicks */
+btnNum1.onclick = function () {
+    btnNum1F();
+  };
+  btnNum2.onclick = function () {
+    btnNum2F();
+  };
+  btnNum3.onclick = function () {
+    btnNum3F();
+  };
+  btnNum4.onclick = function () {
+    btnNum4F();
+  };
+  btnNum5.onclick = function () {
+    btnNum5F();
+  };
+  btnNum6.onclick = function () {
+    btnNum6F();
+  };
+  btnNum7.onclick = function () {
+    btnNum7F();
+  };
+  btnNum8.onclick = function () {
+    btnNum8F();
+  };
+  btnNum9.onclick = function () {
+    btnNum9F();
+  };
+  btnNum0.onclick = function () {
+    btnNum0F();
+  };
+
+  btnLetq.onclick = function () {
+      btnLetqF();
+  }
+  btnLetw.onclick = function () {
+      btnLetwF();
+}
+btnLete.onclick = function () {
+    btnLeteF();
+}
+btnLetr.onclick = function () {
+      btnLetrF();
+}
+btnLett.onclick = function () {
+      btnLettF();
+}
+btnLety.onclick = function () {
+      btnLetyF();
+}
+btnLetu.onclick = function () {
+      btnLetuF();
+}
+btnLeti.onclick = function () {
+      btnLetiF();
+}
+btnLeto.onclick = function () {
+      btnLetoF();
+}
+btnLetp.onclick = function () {
+      btnLetpF();
+}
+btnLeta.onclick = function () {
+      btnLetaF();
+}
+btnLets.onclick = function () {
+    btnLetsF();
+}
+btnLetd.onclick = function () {
+      btnLetdF();
+}
+btnLetf.onclick = function () {
+      btnLetfF();
+}
+btnLetg.onclick = function () {
+      btnLetgF()
+}
+btnLeth.onclick = function () {
+      btnLethF()
+}
+btnLetj.onclick = function () {
+      btnLetjF()
+}
+btnLetk.onclick = function () {
+      btnLetkF()
+}
+btnLetl.onclick = function () {
+      btnLetlF()
+}
+btnLetñ.onclick = function () {
+      btnLetñF()
+}
+btnLetz.onclick = function () {
+      btnLetzF()
+}
+btnLetx.onclick = function () {
+      btnLetxF()
+}
+btnLetc.onclick = function () {
+      btnLetcF()
+}
+btnLetv.onclick = function () {
+      btnLetvF()
+}
+btnLetb.onclick = function () {
+      btnLetbF()
+}
+btnLetn.onclick = function () {
+      btnLetnF()
+}
+btnLetm.onclick = function () {
+      btnLetmF()
+}
+btnComa.onclick = function () {
+    btnComaF()
+}
+btnPunto.onclick = function () {
+    btnPuntoF()
+}
+btnLinea.onclick = function () {
+    btnLineaF()
+}
+btnEspacio.onclick = function () {
+    btnEspacioF()
+}
+btnClear.onclick = function () {
+    btnClearF
+}
+
